87853762c912a7931a67510e18f797e34adf2012
UniverseStark <jty90k@gmail.com>
2020-08-05 18:22:38 +0900

    qwerr
    qwer

--- src/main/java/com/spring/keywar/controller/ControllerMatch.java
+++ src/main/java/com/spring/keywar/controller/ControllerMatch.java
@@ -1,11 +1,16 @@
 package com.spring.keywar.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ControllerMatch {
 
-	
+	@RequestMapping("")
+	public String name() {
+		
+		return "name";
+	}
 	
 	
 }//----
